1b03d05cb8f33623b15df5a30fac7542647b6adf
Ruben <rubenupc.rp@gmail.com>
2020-12-31 20:00:02 +0100

    created homepage, signin and register page

--- header.php
+++ header.php
@@ -41,14 +41,27 @@
                     <a class="nav-link disabled" href="#">Disabled</a>
                 </li>
                 </ul>
+                <div>
+                <a href="signin.php" class="sign-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                        Sign in</a>
+                    <a type="button" class="btn register-btn" href="register.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                        Join us
+                    </a>
+                </div>
+                
+                     <!--
                 <form class="form-inline my-2 my-lg-0">
-                    <a href="#" class="sign-btn"><img src="assets/images/person-fill.svg"/>Sign in</a>
-                    <a type="button" class="btn register-btn" href="#"><img src="assets/images/plus-circle.svg"/> Join us</a>
-                    <!--
                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-                    -->
                 </form>
+                -->
             </div>
         </nav>
     </header>
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php include('header.php');?>
+<div id="page-container">
+   <div id="content-wrap">
+        <p>Welcome!</p>
+   </div>
+</div>
+<?php include('footer.php');?>
